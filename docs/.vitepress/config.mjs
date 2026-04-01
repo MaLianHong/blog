5ffcc575b61ebdcab3e583b9146ef5703e4f5ff0
MaLianHong <71967418+MaLianHong@users.noreply.github.com>
2026-04-01 22:32:52 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/blog/',
   title: "个人网站",
   description: "A VitePress Site",
   themeConfig: {
